76957967a4a1670257d246601036302e27e825ef
pverma04 <parthverma1104@gmail.com>
2022-10-03 21:24:52 -0700

    Project 1 First Commit

---
+++ CSC 203/Project 1/ForestProject_Students/src/Background.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+import processing.core.PImage;
+
+/**
+ * Represents a background for the 2D world.
+ */
+public final class Background
+{
+    public String id;
+    public List<PImage> images;
+    public int imageIndex;
+
+    public Background(String id, List<PImage> images) {
+        this.id = id;
+        this.images = images;
+    }
+    private void setBackground(WorldModel world, Point pos) {
+        if (world.withinBounds(pos)) {
+            this.setBackgroundCell(world, pos);
+        }
+    }
+    private void setBackgroundCell(WorldModel world, Point pos) {
+        world.background[pos.y][pos.x] = this;
+    }
+}
---
+++ CSC 203/Project 1/ForestProject_Students/src/WorldModel.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents the 2D World in which this simulation is running.
+ * Keeps track of the size of the world, the background image for each
+ * location in the world, and the entities that populate the world.
+ */
+public final class WorldModel
+{
+    public int numRows;
+    public int numCols;
+    public Background background[][];
+    public Entity occupancy[][];
+    public Set<Entity> entities;
+
+    public WorldModel(int numRows, int numCols, Background defaultBackground) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.background = new Background[numRows][numCols];
+        this.occupancy = new Entity[numRows][numCols];
+        this.entities = new HashSet<>();
+
+        for (int row = 0; row < numRows; row++) {
+            Arrays.fill(this.background[row], defaultBackground);
+        }
+    }
+    public boolean withinBounds(Point pos) {
+        return pos.y >= 0 && pos.y < this.numRows && pos.x >= 0
+                && pos.x < this.numCols;
+    }
+}
