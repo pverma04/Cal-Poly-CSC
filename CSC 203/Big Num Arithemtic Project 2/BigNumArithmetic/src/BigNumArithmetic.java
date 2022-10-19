31502c99f57eac5a6d34a249ae523ded6a22d6ac
pverma04 <parthverma1104@gmail.com>
2022-10-19 16:10:17 -0700

    Update BigNumArithmetic.java

--- CSC 203/Big Num Arithemtic Project 2/BigNumArithmetic/src/BigNumArithmetic.java
+++ CSC 203/Big Num Arithemtic Project 2/BigNumArithmetic/src/BigNumArithmetic.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,7 +16,7 @@ public class BigNumArithmetic {
      */
     public static void main(String[] args) throws NumberException{
         try {
-            Scanner input = new Scanner(new File());
+            Scanner input = new Scanner(new File("Input"));
             FileWriter output = new FileWriter("Output.txt");
 
             String currentProblem;
@@ -29,40 +30,26 @@ public class BigNumArithmetic {
             operators.add('^');
             while (input.hasNextLine()) {
                 currentProblem = input.nextLine().replaceAll("\\s", ""); //problem with NO spaces
-                int operatorIndex = -1;
                 for(int i = 0; i < currentProblem.length(); i++) {
                     switch (currentProblem.charAt(i)) {
                         case '+':
-                            operatorIndex = i;
-                            operator = '+';
-                            break;
-                        case '*':
-                            operatorIndex = i;
-                            operator = '*';
-                            break;
-                        case '^':
-                            operatorIndex = i;
-                            operator = '^';
-                            break;
-                    }
-                }
-                if (operatorIndex == -1) { //none of the valid operators are present
-                    System.out.println("Invalid problem");
-                    return;
-                } else { //valid problem
-                    firstNum = new Number(currentProblem.substring(0, operatorIndex).replaceFirst("^0+(?!$)", ""));
-                    secondNum = new Number(currentProblem.substring(operatorIndex + 1).replaceFirst("^0+(?!$)", ""));
-                    switch (operator) {
-                        case '+':
+                            firstNum = new Number(currentProblem.substring(0, i).replaceFirst("^0+(?!$)", ""));
+                            secondNum = new Number(currentProblem.substring(i + 1).replaceFirst("^0+(?!$)", ""));
                             answer = firstNum.add(secondNum); //WRITE TO FILE
                             output.write(firstNum.toString() + " + " + secondNum.toString() + " = " + answer.toString());
+                            break;
                         case '*':
+                            firstNum = new Number(currentProblem.substring(0, i).replaceFirst("^0+(?!$)", ""));
+                            secondNum = new Number(currentProblem.substring(i + 1).replaceFirst("^0+(?!$)", ""));
                             answer = firstNum.multiply(secondNum); //WRITE TO FILE
                             output.write(firstNum.toString() + " * " + secondNum.toString() + " = " + answer.toString());
+                            break;
                         case '^':
+                            firstNum = new Number(currentProblem.substring(0, i).replaceFirst("^0+(?!$)", ""));
+                            secondNum = new Number(currentProblem.substring(i + 1).replaceFirst("^0+(?!$)", ""));
                             answer = firstNum.exponent(secondNum); //WRITE TO FILE
-                            output.write(firstNum.toString() + " * " + secondNum.toString() + " = " + answer.toString());
-
+                            output.write(firstNum.toString() + " ^ " + secondNum.toString() + " = " + answer.toString());
+                            break;
                     }
                 }
             }
@@ -89,12 +76,15 @@ public class BigNumArithmetic {
          */
 
 
-//        Number first = new Number("004");
-//        Number second = new Number("005");
-//        Number answer = first.multiply(second);
-//        System.out.println(answer.toString());
+        Number first = new Number("004");
+        Number second = new Number("005");
+        Number answer = first.multiply(second);
+        System.out.println(answer.toString());
+        String currentProblem = "13  * 3";
+        char operator = '*';
 
 
     }
 }
+
 
